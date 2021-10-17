9ab83547ebf1a58cd0bcde21a3e57bc12c4535ab
Piotr J. Tomaszewski <pjtomaszewski98@gmail.com>
2021-10-17 13:58:21 +0200

    Fixed GTK waiting by replacing dbus with dbus-broker

--- Dotfiles/zsh/.zshenv
+++ Dotfiles/zsh/.zshenv
@@ -1,7 +1,3 @@
 export GTK2_RC_FILES=/usr/share/themes/Material-Black-Blueberry-3.38/gtk-2.0/gtkrc
 export BAT_PAGER="/bin/less"
 
-if [ -e /dev/shm/dbus_env.sh ] ; then
-    . /dev/shm/dbus_env.sh
-fi
-
--- Scripts/DevVM/on_ssh_login.sh
+++ Scripts/DevVM/on_ssh_login.sh
@@ -6,12 +6,3 @@ if [ ! -e /tmp/.X11-unix/X5 ] ; then
     socat -b65536 UNIX-LISTEN:/tmp/.X11-unix/X5,fork,mode=777 SOCKET-CONNECT:40:0:x0000x70170000x02000000x00000000 &
 fi
 
-# Fix GTK applications waiting 25 seconds to start
-if [ `ps -ux | grep dbus-daemon | wc -l` -eq 1 ];  then # If current user doesn't have dbus running (one result will be our grep)
-    dbus_params=`dbus-launch`
-    echo '' > /dev/shm/dbus_env.sh
-    while IFS= read -r line; do
-        echo "export $line" >> /dev/shm/dbus_env.sh
-    done <<< "$dbus_params"
-fi
-
