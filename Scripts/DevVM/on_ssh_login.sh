887f8f61721ce4ec6fdc042cca0470b27ae6d377
Piotr J. Tomaszewski <pjtomaszewski98@gmail.com>
2021-10-22 23:04:04 +0200

    Move display forwarding setup to a systemd service

--- Scripts/DevVM/on_ssh_login.sh
+++ Scripts/DevVM/on_ssh_login.sh
@@ -1,8 +1,5 @@
 #! /bin/zsh
 
-# Setup X410 X11 forwarding
+# Setup X11 forwarding
 export DISPLAY=:5.0
-if [ ! -e /tmp/.X11-unix/X5 ] ; then
-    socat -b65536 UNIX-LISTEN:/tmp/.X11-unix/X5,fork,mode=777 SOCKET-CONNECT:40:0:x0000x70170000x02000000x00000000 &
-fi
 
